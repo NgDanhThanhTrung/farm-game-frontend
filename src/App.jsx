eb065e7b6c0566291c782939f2566c428e55725e
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-07-15 23:39:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
-a
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import GameHome from './pages/GameHome';
+import AdminDashboard from './pages/AdminDashboard';
+import RouteGuard from './components/RouteGuard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route Game dành cho User thông thường */}
+        <Route path="/" element={<GameHome />} />
+        
+        {/* Route Guard bảo vệ tài nguyên trang Quản trị viên */}
+        <Route 
+          path="/admin" 
+          element={
+            <RouteGuard>
+              <AdminDashboard />
+            </RouteGuard>
+          } 
+        />
+        
+        {/* Redirect tất cả các đường dẫn lạ về màn hình chính */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
