dab8033263160e9a332cab63387d649111a6d9a7
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-07-15 23:43:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-a
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true, // Cho phép mở giao diện kiểm thử trên điện thoại cùng mạng LAN
+  },
+});
